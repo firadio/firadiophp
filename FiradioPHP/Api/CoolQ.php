e95c458f6777317b0c20d2ae4bb73e6cb92726e1
Xiang/Xisheng <xiangxisheng@gmail.com>
2019-04-17 03:54:51 +0800

    update already

--- FiradioPHP/Api/CoolQ.php
+++ FiradioPHP/Api/CoolQ.php
@@ -14,9 +14,8 @@ class CoolQ {
     private $aConfig;
     private $oCurl;
     public $oHttpClient;
-    private $msglimit = array();
 
-    public function __construct($conf) {
+    public function __construct($conf = array()) {
         $this->aConfig = $conf;
     }
 
@@ -30,40 +29,44 @@ class CoolQ {
         $this->oHttpClient->setHeader('Authorization', 'token ' . $sToken);
     }
 
+    private function oHttpClient() {
+        return $this->oCurl;
+    }
+
+    private function getData($path, $post) {
+        $ret = $this->oHttpClient()->post_json($path, $post);
+        $ret = json_decode($ret, true);
+        if (!is_array($ret)) {
+            return array('message' => 'is not a array');
+        }
+        if (!isset($ret['data'])) {
+            return $ret;
+        }
+        return $ret['data'];
+    }
+
     /**
      * 发送私聊消息
      * @param int $qquin
      * @param string $message
      * @param bool $is_raw
      */
-    public function send_private_msg($qquin, $message, $is_raw = false) {
-        $msglimit_str = $message;
-        $msgtime = isset($this->msglimit[$msglimit_str]) ? $this->msglimit[$msglimit_str] : 0;
-        if (time() - $msgtime < 60) {
-            //相同的$message间隔时间不能小于60秒
-            return;
-        }
-        $this->msglimit[$msglimit_str] = time();
+    public function send_private_msg($qquin, $message, $auto_escape = false) {
         $post = array();
         $post['user_id'] = $qquin;
         $post['message'] = $message;
-        $post['is_raw'] = $is_raw;
-        $ret = $this->oHttpClient->post_json('/send_private_msg', $post);
+        $post['auto_escape'] = $auto_escape;
+        $data = $this->getData('/send_private_msg', $post);
+        return $data;
     }
 
-    public function send_group_msg($group_id, $message, $is_raw = false) {
-        $msglimit_str = $message;
-        $msgtime = isset($this->msglimit[$msglimit_str]) ? $this->msglimit[$msglimit_str] : 0;
-        if (time() - $msgtime < 60) {
-            //相同的$message间隔时间不能小于60秒
-            return;
-        }
-        $this->msglimit[$msglimit_str] = time();
+    public function send_group_msg($group_id, $message, $auto_escape = false) {
         $post = array();
         $post['group_id'] = $group_id;
         $post['message'] = $message;
-        $post['is_raw'] = $is_raw;
-        $ret = $this->oHttpClient->post_json('/send_group_msg', $post);
+        $post['auto_escape'] = $auto_escape;
+        $data = $this->getData('/send_group_msg', $post);
+        return $data;
     }
 
     public function set_friend_add_request($flag, $approve = true, $remark = NULL, $fCallBack = NULL) {
@@ -71,17 +74,33 @@ class CoolQ {
         $post['flag'] = $flag;
         $post['approve'] = $approve;
         $post['remark'] = $remark;
-        $ret = $this->oCurl->post('/set_friend_add_request', $post);
-        //$ret = $this->oHttpClient->post_json('/set_friend_add_request', $post, $fCallBack);
+        $ret = $this->oHttpClient()->post('/set_friend_add_request', $post);
+        //$ret = $this->oHttpClient()->post_json('/set_friend_add_request', $post, $fCallBack);
     }
 
     public function get_stranger_info($user_id, $no_cache = FALSE) {
         $post = array();
         $post['user_id'] = $user_id;
         $post['no_cache'] = $no_cache;
-        $ret = $this->oCurl->post('/get_stranger_info', $post);
+        $ret = $this->oHttpClient()->post('/get_stranger_info', $post);
         $ret = json_decode($ret, true);
         return $ret;
+    }
+
+    public function delete_msg($message_id) {
+        $post = array();
+        $post['message_id'] = $message_id;
+        $data = $this->getData('/delete_msg', $post);
+        return $data;
+    }
+
+    public function set_group_ban($group_id, $user_id, $duration) {
+        $post = array();
+        $post['group_id'] = $group_id;
+        $post['user_id'] = $user_id;
+        $post['duration'] = $duration;
+        $data = $this->getData('/set_group_ban', $post);
+        return $data;
     }
 
 }
