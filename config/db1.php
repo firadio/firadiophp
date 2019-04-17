16db42b4afafe29e179731779d6b9ea07ce4f331
Xiang/Xisheng <xiangxisheng@gmail.com>
2019-04-17 18:59:36 +0800

    mysql ok

--- config/db1.php
+++ config/db1.php
@@ -2,11 +2,11 @@
 
 return function() {
     $config = require(__DIR__ . DS . 'db' . DS . 'default.php');
-    $config['host'] = 'localhost';
-    $config['port'] = '3306';
+    $config['host'] = 'vps.firadio.net';
+    $config['port'] = '3324';
     $config['dbname'] = 'information_schema';
     $config['tablepre'] = '';
-    $config['username'] = 'root';
-    $config['password'] = '123456';
+    $config['username'] = 'feieryun';
+    $config['password'] = 'feieryun';
     return $config;
 };
