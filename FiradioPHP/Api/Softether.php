5aae0f46e37670f54fdbb2f50f20a99f9d365b8b
Xiang/Xisheng <xiangxisheng@gmail.com>
2019-09-15 20:31:16 +0800

    Softether

--- FiradioPHP/Api/Softether.php
+++ FiradioPHP/Api/Softether.php
@@ -19,6 +19,7 @@ class Softether {
         //$this->oCurl->setHeader('Content-Type', 'application/json');
         $this->oCurl->setHeader('X-VPNADMIN-HUBNAME', $this->aConfig['hubname']);
         $this->oCurl->setHeader('X-VPNADMIN-PASSWORD', $this->aConfig['password']);
+        $this->aCache['HubName_str'] = 'DEFAULT';
     }
 
     private function api($method, $params = array()) {
@@ -45,21 +46,63 @@ class Softether {
         return $this->api('Test', $params);
     }
 
-    public function GetUser($Name_str, $HubName_str = 'DEFAULT') {
+    public function SetHubName($HubName_str) {
+        $this->aCache['HubName_str'] = $HubName_str;
+    }
+
+    public function GetDefaParams($HubName_str = NULL) {
         $params = array();
-        $params['HubName_str'] = $HubName_str;
+        $params['HubName_str'] = ($HubName_str === NULL) ? $this->aCache['HubName_str'] : $HubName_str;
+        return $params;
+    }
+
+    public function CreateHub($HubName_str = NULL) {
+        $params = $this->GetDefaParams($HubName_str);
+        $params['Online_bool'] = true;
+        $params['NoEnum_bool'] = true;
+        return $this->api('CreateHub', $params);
+    }
+
+    public function CreateGroup($Name_str) {
+        $params = $this->GetDefaParams();
+        $params['Name_str'] = $Name_str;
+        return $this->api('CreateGroup', $params);
+    }
+
+    public function SetGroup($Name_str) {
+        $params = $this->GetDefaParams();
+        $params['Name_str'] = $Name_str;
+        $params['UsePolicy_bool'] = true;
+        $params['policy:Access_bool'] = true;
+        //$params['policy:DHCPFilter_bool'] = true;
+        $params['policy:DHCPNoServer_bool'] = true;
+        $params['policy:DHCPForce_bool'] = true;
+        $params['policy:NoRouting_bool'] = true;
+        $params['policy:CheckMac_bool'] = true;
+        $params[''] = true;
+        $params[''] = true;
+        $params[''] = true;
+        $params[''] = true;
+        $params[''] = true;
+        $params[''] = true;
+        $params[''] = true;
+        $params[''] = true;
+        return $this->api('SetGroup', $params);
+    }
+
+    public function GetUser($Name_str, $HubName_str = NULL) {
+        $params = $this->GetDefaParams($HubName_str);
         $params['Name_str'] = $Name_str;
         return $this->api('GetUser', $params);
     }
 
-    public function CreateUser($Name_str, $HubName_str = 'DEFAULT') {
-        $params = array();
-        $params['HubName_str'] = $HubName_str;
+    public function CreateUser($Name_str, $Auth_Password_str, $dayLength = 30) {
+        $params = $this->GetDefaParams();
         $params['Name_str'] = $Name_str;
         $params['AuthType_u32'] = 1;
-        $params['Auth_Password_str'] = $Name_str;
+        $params['Auth_Password_str'] = $Auth_Password_str;
         $params['GroupName_str'] = 'user';
-        $params['ExpireTime_dt'] = gmdate('c', time() + 365 * 86400);
+        $params['ExpireTime_dt'] = gmdate('c', time() + $dayLength * 86400);
         return $this->api('CreateUser', $params);
     }
 
