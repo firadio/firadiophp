ef0b6c1aaef4a828c2a27f57c76206407c73ba8e
Xiang/Xisheng <xiangxisheng@gmail.com>
2020-02-23 17:30:58 +0800

    .secret

--- github/aliyun-openapi-php-sdk.php
+++ github/aliyun-openapi-php-sdk.php
@@ -3,10 +3,14 @@
 include_once (__DIR__) . '/github.com/aliyun/aliyun-openapi-php-sdk/aliyun-php-sdk-core/Config.php';
 use Ecs\Request\V20140526 as Ecs;
 
-$iClientProfile = DefaultProfile::getProfile("cn-hangzhou", "<your accessKey>", "<your accessSecret>");
+$regionId = 'cn-huhehaote';
+$accessKeyId = '';
+$accessSecret = '';
+require(__DIR__ . '/aliyun-openapi-php-sdk.secret~');
+$iClientProfile = DefaultProfile::getProfile($regionId, $accessKeyId, $accessSecret);
 $client = new DefaultAcsClient($iClientProfile);
 
 $request = new Ecs\DescribeRegionsRequest();
 $request->setMethod("GET");
 $response = $client->getAcsResponse($request);
-print_r($response);
+file_put_contents('aliyun-openapi-php-sdk.json~', json_encode($response));
