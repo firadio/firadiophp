656c615d8fbac74850d0d1c27eec834642eac0a2
Xiang/Xisheng <xiangxisheng@gmail.com>
2020-10-03 02:27:52 +0800

    keypre

--- FiradioPHP/Database/Redis.php
+++ FiradioPHP/Database/Redis.php
@@ -6,9 +6,11 @@ class Redis {
 
     private $config = array();
     private $redis;
+    private $keypre = '';
 
     public function __construct(array $setting) {
         $this->config = $setting['config'];
+        $this->keypre = isset($this->config['keypre']) ? $this->config['keypre'] : '';
         $this->connect();
     }
 
@@ -25,68 +27,68 @@ class Redis {
             if (count($value) === 0) {
                 return;
             }
-            array_unshift($value, $name);
+            array_unshift($value, $this->keypre . $name);
             return call_user_func_array([$this->redis, 'sAdd'], $value);
         }
-        return $this->redis->sAdd($name, $value);
+        return $this->redis->sAdd($this->keypre . $name, $value);
     }
 
     public function sCard($name) {
         // 获得总数
-        return $this->redis->sCard($name);
+        return $this->redis->sCard($this->keypre . $name);
     }
 
     public function del($name) {
-        return $this->redis->del($name);
+        return $this->redis->del($this->keypre . $name);
     }
 
     public function sRandMember($name, $count = 1) {
-        return $this->redis->sRandMember($name, $count);
+        return $this->redis->sRandMember($this->keypre . $name, $count);
     }
 
     public function set($name, $value, $param3 = NULL) {
         // 参考 https://github.com/phpredis/phpredis/#set
         if ($param3 !== NULL) {
-            return $this->redis->set($name, $value, $param3);
+            return $this->redis->set($this->keypre . $name, $value, $param3);
         }
-        return $this->redis->set($name, $value);
+        return $this->redis->set($this->keypre . $name, $value);
     }
 
     public function get($name) {
-        return $this->redis->get($name);
+        return $this->redis->get($this->keypre . $name);
     }
 
     public function delKeys($keyname) {
-        $keys = $this->redis->KEYS($keyname);
+        $keys = $this->redis->KEYS($this->keypre . $keyname);
         foreach ($keys as $key) {
             $this->redis->DEL($key);
         }
     }
 
     public function HSET($name, $key, $val) {
-        return $this->redis->HSET($name, $key, $val);
+        return $this->redis->HSET($this->keypre . $name, $key, $val);
     }
 
     public function HGET($name, $key) {
-        return $this->redis->HGET($name, $key);
+        return $this->redis->HGET($this->keypre . $name, $key);
     }
 
     public function HDEL($name, $key) {
-        return $this->redis->HDEL($name, $key);
+        return $this->redis->HDEL($this->keypre . $name, $key);
     }
 
     public function Keys($keyname) {
-        $keys = $this->redis->KEYS($keyname);
+        $keys = $this->redis->KEYS($this->keypre . $keyname);
         return $keys;
     }
 
     public function zCount($keyname, $start, $end) {
-        $i = $this->redis->zCount($keyname, $start, $end);
+        $i = $this->redis->zCount($this->keypre . $keyname, $start, $end);
         return $i;
     }
 
     public function hGetAll($keyname) {
-        $row = $this->redis->hGetAll($keyname);
+        $row = $this->redis->hGetAll($this->keypre . $keyname);
         if ($row === FALSE) {
             $row = array();
         }
