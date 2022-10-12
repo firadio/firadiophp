a08a9c4d424c4493268ad78ba69450ae4bd7c00f
firadio <www@firadio.com>
2022-10-12 19:50:40 +0800

    lock

--- FiradioPHP/System/Cli.php
+++ FiradioPHP/System/Cli.php
@@ -12,9 +12,45 @@ class Cli {
 
     private $filepath;
     private $lockfile_handle;
-    public function __construct() {
+    private $rootdir;
+    public function __construct($rootdir = __DIR__ . '/lock') {
         echo "[Cli] Start...\r\n";
+        $this->rootdir = $rootdir;
+        if (!file_exists($this->rootdir)) {
+            mkdir($this->rootdir);
+        }
+    }
 
+    public function getUnLockIds() {
+        $aUnLockIds = array();
+        if ($handle = opendir($this->rootdir)) {
+            while (false !== ($entry = readdir($handle))) {
+                $filepath = $this->rootdir . '/' . $entry;
+                if (is_file($filepath)) {
+                    $lockfile_handle = fopen($filepath, 'w+');
+                    if (flock($lockfile_handle, LOCK_EX | LOCK_NB)) {
+                        fclose($lockfile_handle);
+                        $aUnLockIds[] = pathinfo($filepath, PATHINFO_FILENAME);
+                    }
+                }
+            }
+        }
+        return $aUnLockIds;
+    }
+
+    public function delLockId($lock_id) {
+        $filepath = $this->rootdir . '/' . $lock_id . '.lock';
+        $lockfile_handle = fopen($filepath, 'w+');
+        if (flock($lockfile_handle, LOCK_EX | LOCK_NB)) {
+            unlink($filepath);
+        }
+    }
+
+    public function newLockId() {
+        //生成新的锁ID
+        $lock_id = mt_rand(100000, 999999);
+        $this->lock($this->rootdir . '/' . $lock_id);
+        return $lock_id;
     }
 
     public function lock($filepath) {
