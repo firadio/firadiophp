80c55972b9702cbe1eafbbbffb4e9e4b279d31c2
firadio <www@firadio.com>
2023-06-26 18:19:10 +0800

    tx

--- git_push_tencent.bat
+++ git_push_tencent.bat
@@ -5,7 +5,7 @@ SET path=%path%;E:\data\program\PortableGit\bin\
 SET path=%path%;C:\data\program\PortableGit\bin\
 ECHO =========git push tencent master============
 git remote remove tencent
-git remote add tencent https://git.code.tencent.com/firadio/firadiophp
+git remote add tencent git@git.code.tencent.com:firadio/firadiophp.git
 git pull tencent master
 git push tencent master
 PAUSE
